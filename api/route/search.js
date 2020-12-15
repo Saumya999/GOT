1cc2f2372b4b0ad47253e1348c4d89a736e304c7
Saumya999 <saumyadiptasarkar49@gmail.com>
2020-12-16 01:52:51 +0530

    Added Search enhancement for location based Search

--- api/route/search.js
+++ api/route/search.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const BattelData = require("../models/data");
 
 router.get("/search", (req, res, next) => {
+
   if (req.query.location && req.query.type) {
+    console.log("Hello 1");
     BattelData.find({
       $or: [
         { attacker_king: req.query.king },
@@ -19,8 +21,17 @@ router.get("/search", (req, res, next) => {
           response: data,
         });
       });
-  }
-  else {
+  } else if (req.query.location) {
+    console.log(req.query.location);
+    BattelData.find({ location: req.query.location })
+      .exec()
+      .then((data) => {
+        res.status(200).json({
+          response: data,
+        });
+      });
+  } else {
+    console.log("Hello 2");
     BattelData.find({
     $or: [{ attacker_king: req.query.king }, { defender_king: req.query.king }],
   })
